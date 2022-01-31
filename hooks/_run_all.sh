b55bb4959c90788341ef10a55939f6550553240c
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-01-31 12:31:30 +0100

    adding explanation in comments

--- hooks/_run_all.sh
+++ hooks/_run_all.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-paths=(src)
+paths=(src)  # If you wish to check more directories, you may add them to this list.
+
 for path in "${paths[@]}"; do
     for script in $(find hooks -type f -name "[^_]*.sh"); do
         $script $path
