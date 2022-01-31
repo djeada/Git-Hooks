2ab7fb70f1c319acdb64e2fc653f62a17b044803
djeada <addjellouli1@gmail.com>
2022-01-31 19:42:01 +0100

    correct formatting of _run_all.sh

--- hooks/_run_all.sh
+++ hooks/_run_all.sh
@@ -3,8 +3,8 @@
 paths=(src)  # If you wish to check more directories, you may add them to this list.
 
 for path in "${paths[@]}"; do
-    for script in $(find hooks -type f -name "[^_]*.sh"); do
-      $("$script" "$path")
+  for script in $(find "$path" -type f -name "[^_]*.sh"); do
+      $("$script $path")
       echo "Executing "$script""
     done
 done
