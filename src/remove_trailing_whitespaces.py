8a27a7864ab4e3dc407ffbf63ce1e9db6a9506d1
djeada <addjellouli1@gmail.com>
2022-02-05 13:53:58 +0100

    adding remove trailing whitespaces scripts

--- src/remove_trailing_whitespaces.py
+++ src/remove_trailing_whitespaces.py
@@ -1,1 +1,35 @@
+import sys
+import pathlib
 
+def remove_trailing_whitespaces(file_name: str) -> None:
+    # read file contents
+    with open(file_name, "r", encoding="utf-8") as file:
+        contents = file.read()
+
+    # check each line for trailing whitespaces
+    contents = [line.rstrip() for line in contents.splitlines()]
+
+    # write file contents
+    with open(file_name, "w", encoding="utf-8") as file:
+        file.write("\n".join(contents))
+
+
+if __name__ == "__main__":
+
+    # check if user provided file name
+    if len(sys.argv) != 2:
+        print("Usage: python remove_trailing_whitespaces.py <dir_name>")
+        exit()
+
+    # check if file exists
+    file_name = sys.argv[1]
+
+    if not pathlib.Path(file_name).is_dir():
+        print("Dir does not exist")
+        exit()
+
+    # find all files in directory and remove diacritics
+    for file in pathlib.Path(file_name).glob("**/*"):
+        if file.is_file():
+            remove_trailing_whitespaces(file)
+            
--- src/remove_trailing_whitespaces.sh
+++ src/remove_trailing_whitespaces.sh
@@ -1,1 +1,46 @@
+#!/usr/bin/env bash
+
+remove_trailing_whitespaces() {
+    local file="$1"
+
+    echo "Checking each line of ${file} for trailing whitespaces..."
+
+    while IFS= read -r line; do
+        if [[ $line == *[[:space:]]* ]]; then
+            echo "Found trailing whitespaces in line: ${line}"
+            echo "${line}" | sed -e 's/[[:space:]]*$//'
+        else
+            echo "${line}"
+        fi
+    done < "${file}" > "${file}.tmp"
+
+    mv "${file}.tmp" "${file}"
+
+    echo "Done!"
+}
+
+main() {
+
+    if [ $# -eq 0 ]; then
+        echo "Must provide a path!"
+        exit 1
+    elif [ $# -gt 1 ]; then
+        echo "Only one path is supported!"
+        exit 1
+    fi
+
+    if [ "$1" == '.' ] || [ -d "${1}" ]; then
+        for file in $(find $1 -maxdepth 10 -type f)
+        do
+            remove_trailing_whitespaces $file
+        done
+    elif [ -f "${1}" ]; then
+        remove_trailing_whitespaces $1
+    else
+        echo "$1 is not a valid path!"
+    fi
+
+}
+
+main "$@"
 
