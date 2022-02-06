abe7ac8842649e9632a99e6cef9275f4a037c6d5
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-02-07 00:26:58 +0100

    Update python_format.sh

--- src/python_format.sh
+++ src/python_format.sh
@@ -1,9 +1,29 @@
 #!/usr/bin/env bash
 
+format() {
+    black "$1"/**/*.sh
+    autopep8 "$1"/**/*.sh
+}
+
 main() {
 
-    black **/*.py
-    autopep8 **/*.py
+
+    if [ $# -eq 0 ]; then
+        echo "Must provide a path!"
+        exit 1
+    fi
+
+    if [ $1 == '.' ] || [ -d "${1}" ]; then
+        for file in $(find $1 -maxdepth 10 -type f)
+        do
+            format $file
+        done
+    elif [ -f "${1}" ]; then
+        format $1
+    else
+        echo "$1 is not a valid path!"
+    fi
+
 
 }
 
